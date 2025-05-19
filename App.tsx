06fb13543e7f8cbc07d1e3d92a55297eff303dba
Miguel <126696785+MiguelVascoDevWeb@users.noreply.github.com>
2025-05-19 19:57:44 -0300

    Navegação Landing Page - LogIn
    - Navegação funcional da Landing Page para a tela de LogIn
    - Adição do .gitignore

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import LogInScreen from './src/screens/LogInSignUp/LogInScreen';
 import LandingScreen from './src/screens/LandingScreen';
-import StackNavigation from './src/navigation/StackNavigation'
+import StackNavigation from './src/navigation/StackNavigation';
+import { NavigationContainer } from '@react-navigation/native';
 
 
 export default function App() {
   return (
-    <StackNavigation></StackNavigation>
+    <NavigationContainer>
+      <StackNavigation></StackNavigation>
+    </NavigationContainer>
   );
 }
 
