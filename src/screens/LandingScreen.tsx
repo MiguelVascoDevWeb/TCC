ee68a45e3e5428ffce99ca9822249d6e92f6a142
Miguel <miguelvasconcelos.7a.cng@gmail.com>
2025-06-09 12:32:30 -0300

    Landing Page atualização

--- src/screens/LandingScreen.tsx
+++ src/screens/LandingScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity, Button} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../src/navigation/StackNavigation';
 
@@ -9,21 +10,25 @@ export default function LandingScreen({ navigation }: Props) {
 
     return(
         <View style={style.container}>
-            <View style={style.viewTop}>
+            <LinearGradient 
+                colors={['#41D499', '#82EAC1']} start={{ x: 0, y: 0.75 }} end={{x: 0, y: 0.99}} 
+                style={style.viewTop}
+            >
                 <Image source={{uri: 'https://reactjs.org/logo-og.png'}}
                     style={{width: 50, height: 50}} />
-            </View>
+            </LinearGradient>
             <View style={style.viewBottom}>
                 <TouchableOpacity style={style.button} 
                     onPress={() => navigation.navigate('LogIn')}
                 >
-                    <Text>Continuar</Text>
+                    <Text style={style.buttonText}>Continuar</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={style.button}
                     onPress={() => navigation.navigate('SignUp')}
                 >
-                    <Text>Cadastro</Text>
+                    <Text style={style.buttonText} >Cadastro</Text>
                 </TouchableOpacity>
+                
             </View>
         </View>
     );
@@ -31,7 +36,8 @@ export default function LandingScreen({ navigation }: Props) {
 
 const style = StyleSheet.create({
     container: {
-        height:'100%'
+        height:'100%',
+        backgroundColor: '#FAFEFC',
     },
 
     viewTop: {
@@ -45,10 +51,19 @@ const style = StyleSheet.create({
         alignItems: 'center',
         justifyContent:'center',
         height:'30%',
+        elevation: 5
     },
 
     button: {
-        backgroundColor: 'green',
+        paddingVertical: 10,
+        paddingHorizontal:70,
+        borderRadius: 25,
+        backgroundColor: '#41D499',
+        marginVertical: 5,
+    },
+    
+    buttonText: {
+        color: '#FAFEFC'
     }
 
     
